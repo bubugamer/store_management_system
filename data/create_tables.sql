f795a54944f590d5aa426df9046710dd6b40bccc
Meansss <geebeeboper@gmail.com>
2023-08-02 15:33:10 +0800

    更新建表脚本

--- data/create_tables.sql
+++ data/create_tables.sql
@@ -1,38 +1,27 @@
-CREATE TABLE products (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name TEXT NOT NULL,
-    category TEXT NOT NULL,
-    brand TEXT,
-    quantity INTEGER,
-    cost_price REAL,
-    supplier TEXT,
-    unit TEXT,
-    specification TEXT,
-    barcode TEXT,
-    remark TEXT,
-    attribute1 TEXT,
-    attribute2 TEXT,
-    attribute3 TEXT,
-    attribute4 TEXT,
-    attribute5 TEXT,
-    in_time DATETIME,
-    out_time DATETIME
-);
-
-CREATE TABLE sales (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    salesperson TEXT NOT NULL,
-    sale_date TEXT NOT NULL,
-    price REAL NOT NULL,
-    product_id INTEGER NOT NULL,
-    FOREIGN KEY(product_id) REFERENCES product(id)
-);
-
-CREATE TABLE employees (
-    name TEXT NOT NULL,
-    address TEXT,
-    phone TEXT,
-    PRIMARY KEY (name)
-);
+CREATE TABLE "products" (
+	"id"	INTEGER UNIQUE,
+	"name"	TEXT NOT NULL,
+	"category"	TEXT NOT NULL,
+	"brand"	TEXT,
+	"status"	INTEGER NOT NULL,
+	"cost_price"	REAL,
+	"supplier"	TEXT,
+	"list_price"	REAL,
+	"sales_price"	REAL,
+	"salesperson"	TEXT,
+	"unit"	TEXT,
+	"specification"	TEXT,
+	"barcode"	TEXT,
+	"remark"	TEXT,
+	"attribute1"	TEXT,
+	"attribute2"	TEXT,
+	"attribute3"	TEXT,
+	"attribute4"	TEXT,
+	"attribute5"	TEXT,
+	"in_time"	DATETIME,
+	"out_time"	DATETIME,
+	"abandonment_time"	DATETIME,
+	PRIMARY KEY("id" AUTOINCREMENT)
+)
 
 commit;
